4e4af862387ada2a0fb9dc24e0fcbd7b4e82f363
ssq0-0 <snikers1kitkat2@gmail.com>
2024-07-04 18:09:15 +0300

    Starting work on the project. Connecting the database and starting the server. In the future the connection will be converted to read passwords, names and other things from a third-party file, for this purpose methods will be added.

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,9 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"InterestingChats/internal/db"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
+	cfg := db.DataBaseConfig{Host: "localhost", Port: 5432, User: "***", Password: "***", Dbname: "***"}
+	conn, err := db.Connect(cfg)
+	if err != nil {
+		log.Printf("Unable to connect to database: %v\n", err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Successfully connected to the database!")
+	log.Println("Starting server on :8080")
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
+		log.Fatalf("Unable to start server: %v\n", err)
 	}
 }
