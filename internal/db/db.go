1ff0b19dad5b726295460351ca1b559fb175606a
ssq0-0 <snikers1kitkat2@gmail.com>
2024-07-05 18:12:00 +0300

    Delete comments

--- internal/db/db.go
+++ internal/db/db.go
@@ -15,18 +15,6 @@ type DataBaseConfig struct {
 	Dbname   string
 }
 
-func (db *DataBaseConfig) LoadConfig() {
-	// db.Host = "localhost"
-	// db.Port = 5432
-	// db.User = ";;;"
-	// db.Password = ";;;"
-	// db.Dbname = ";;;"
-}
-
-// func (db *DataBaseConfig) Close() error {
-// 	return db.Close()
-// }
-
 func Connect(dbConf DataBaseConfig) (*sql.DB, error) {
 	connectStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", dbConf.Host, dbConf.Port, dbConf.User, dbConf.Password, dbConf.Dbname)
 	db, err := sql.Open("postgres", connectStr)
