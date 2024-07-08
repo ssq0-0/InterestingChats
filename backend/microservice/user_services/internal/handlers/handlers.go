a1c7fb63e1071385b18115bfeaae8074f8c9d5d7
ssq0-0 <snikers1kitkat2@gmail.com>
2024-07-08 20:41:44 +0300

    added login function

--- backend/microservice/user_services/internal/handlers/handlers.go
+++ backend/microservice/user_services/internal/handlers/handlers.go
@@ -13,33 +13,31 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-type Handler struct {
-	// Db *sql.DB
-}
+type Handler struct{}
 
 func NewHandler() *Handler {
-	return &Handler{
-		// Db: db,
-	}
+	return &Handler{}
 }
 
 func (h *Handler) Registrations(w http.ResponseWriter, r *http.Request) {
 	var u models.User
 	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, "Problems with decode data", http.StatusBadRequest)
+		fmt.Println("Problems with decode data")
 		return
 	}
 
 	hashPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, "Problems with generate password", http.StatusBadRequest)
 		fmt.Println("Problems with generate password")
 		return
 	}
+	fmt.Println(string(hashPassword))
 
 	accessToken, refreshToken, err := utils.GenerateJWT(u.Username)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, "Problems with generate JWT", http.StatusBadRequest)
 		fmt.Println("Problems with generate JWT")
 		return
 	}
@@ -49,7 +47,7 @@ func (h *Handler) Registrations(w http.ResponseWriter, r *http.Request) {
 		"email":    u.Email,
 		"password": hashPassword,
 	}
-	body, statusCode, err := sendRequestToDBServer(data)
+	body, statusCode, err := sendRequestToDBServer(data, "registration")
 	if err != nil {
 		http.Error(w, "Failed to create user in database", statusCode)
 		return
@@ -67,13 +65,53 @@ func (h *Handler) Registrations(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
-func sendRequestToDBServer(data map[string]interface{}) ([]byte, int, error) {
+func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
+	var u models.User
+	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+		http.Error(w, "Failed to parse json data", http.StatusBadRequest)
+		return
+	}
+
+	data := map[string]interface{}{
+		"email":    u.Email,
+		"password": u.Password,
+	}
+	body, statusCode, err := sendRequestToDBServer(data, "login")
+	if err != nil {
+		http.Error(w, "Failed to login.", statusCode)
+		w.WriteHeader(statusCode)
+		return
+	}
+	if statusCode != http.StatusAccepted {
+		http.Error(w, string(body), statusCode)
+		return
+	}
+
+	accessToken, refreshToken, err := utils.GenerateJWT(u.Username)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		fmt.Println("Problems with generate JWT")
+		return
+	}
+
+	response := map[string]string{
+		"body":         string(body),
+		"accessToken":  accessToken,
+		"refreshToken": refreshToken,
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+
+}
+
+func sendRequestToDBServer(data map[string]interface{}, target string) ([]byte, int, error) {
 	jsonReqData, err := json.Marshal(data)
 	if err != nil {
 		return nil, http.StatusInternalServerError, fmt.Errorf("failed to serialize data: %w", err)
 	}
-
-	resp, err := http.Post("http://localhost:8002/registration", "application/json", bytes.NewBuffer(jsonReqData))
+	urlPOST := fmt.Sprintf("http://localhost:8002/%s", target)
+	resp, err := http.Post(urlPOST, "application/json", bytes.NewBuffer(jsonReqData))
 	if err != nil {
 		return nil, http.StatusInternalServerError, fmt.Errorf("failed to send request: %w", err)
 	}
@@ -86,84 +124,3 @@ func sendRequestToDBServer(data map[string]interface{}) ([]byte, int, error) {
 
 	return body, resp.StatusCode, nil
 }
-
-// func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
-// 	var u models.User
-// 	fmt.Println("Login...")
-// 	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
-// 		http.Error(w, err.Error(), http.StatusBadRequest)
-// 	}
-// 	fmt.Println("Login...")
-
-// 	userService := db.NewUserService(h.Db)
-// 	boolCheck, accessToken, refreshToken, err := userService.LoginData(r.Context(), u)
-// 	if err != nil && !boolCheck {
-// 		http.Error(w, err.Error(), http.StatusBadRequest)
-// 		return
-// 	}
-// 	fmt.Println("Login...")
-
-// 	response := map[string]string{
-// 		"message":      "Successful; login!",
-// 		"accessToken":  accessToken,
-// 		"refreshToken": refreshToken,
-// 	}
-// 	fmt.Println("Login...")
-
-// 	// w.WriteHeader(http.StatusOK)
-// 	// http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
-// 	w.WriteHeader(http.StatusCreated)
-// 	json.NewEncoder(w).Encode(response)
-// 	fmt.Println("Login...")
-
-// }
-
-// func (h *Handler) RefreshAccessToken(w http.ResponseWriter, r *http.Request) {
-
-// }
-
-// func (h *Handler) UpdateAccessToken(w http.ResponseWriter, r *http.Request) {
-// 	body, err := ioutil.ReadAll(r.Body)
-// 	if err != nil {
-// 		http.Error(w, "Unable to read request body", http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	var data map[string]string
-// 	if err := json.Unmarshal(body, &data); err != nil {
-// 		http.Error(w, "Invalid json format", http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	refreshToken, exists := data["refreshToken"]
-// 	if !exists {
-// 		http.Error(w, "Missing refreshToken in request body", http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	userService := db.NewUserService(h.Db)
-// 	tokenExists, err := userService.CheckToken(r.Context(), refreshToken)
-// 	if err != nil {
-// 		http.Error(w, "Can't search token", http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	if !tokenExists {
-// 		http.Error(w, "Invalid token", http.StatusUnauthorized)
-// 		return
-// 	}
-
-// 	newAccessToken, err := utils.RefreshToken(refreshToken)
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	response := map[string]string{
-// 		"message":     "update token!",
-// 		"accessToken": newAccessToken,
-// 	}
-
-// 	w.WriteHeader(http.StatusCreated)
-// 	json.NewEncoder(w).Encode(response)
-// }
