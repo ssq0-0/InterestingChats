9743abf60a63fe66a3e219e5578d1a35ec266d46
ssq0-0 <snikers1kitkat2@gmail.com>
2024-07-12 00:08:23 +0300

    new logger. 'fmt' delete

--- backend/microservice/db/internal/handlers/user/user_handlers.go
+++ backend/microservice/db/internal/handlers/user/user_handlers.go
@@ -5,7 +5,7 @@ import (
 	"InterestingChats/backend/microservice/db/internal/models"
 	"database/sql"
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 
 	"golang.org/x/crypto/bcrypt"
@@ -25,7 +25,7 @@ func (h *Handler) Registrations(w http.ResponseWriter, r *http.Request) {
 	var u models.User
 	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
-		fmt.Println("Can't parse user data from request")
+		log.Println("Can't parse user data from request: ", err)
 		return
 	}
 
@@ -33,7 +33,7 @@ func (h *Handler) Registrations(w http.ResponseWriter, r *http.Request) {
 	err := userService.CreateNewUser(r.Context(), u)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
-		fmt.Println("Can't exec user in database")
+		log.Println("Can't exec user in database: ", err)
 		return
 	}
 	response := map[string]string{
@@ -46,7 +46,7 @@ func (h *Handler) Registrations(w http.ResponseWriter, r *http.Request) {
 func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
 	var u models.User
 	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
-		fmt.Println("error decode", err)
+		log.Println("error decode: ", err)
 		http.Error(w, "Failed to decode user login data", http.StatusBadRequest)
 		return
 	}
@@ -54,16 +54,17 @@ func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
 	userService := db.NewUserService(h.Db)
 	dbPassword, err := userService.LoginData(r.Context(), u)
 	if err != nil {
-		fmt.Println("Incorrect user data decode", err)
-		http.Error(w, "Incorrect user data", http.StatusBadRequest)
+		log.Println("Incorrect user data decode: ", err)
+		http.Error(w, "Incorrect user data:", http.StatusBadRequest)
 		return
 	}
-	if err := bcrypt.CompareHashAndPassword([]byte(dbPassword), []byte(u.Pasword)); err != nil {
+	if err := bcrypt.CompareHashAndPassword([]byte(dbPassword), []byte(u.Password)); err != nil {
+		log.Println("Incoerrect email or password:", err)
 		http.Error(w, "Incorrect email or password", http.StatusBadRequest)
 		return
 	}
 	response := map[string]string{
-		"message:": "Successful login!",
+		"message": "Successful login!",
 	}
 	w.WriteHeader(http.StatusAccepted)
 	json.NewEncoder(w).Encode(response)
