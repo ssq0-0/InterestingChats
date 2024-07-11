36c181d5f730154f7d830f9e64bf290ce71bc212
ssq0-0 <snikers1kitkat2@gmail.com>
2024-07-11 23:59:18 +0300

    A new route has been added. fmt changed to usual logging

--- backend/microservice/api_gateway/internal/server/server.go
+++ backend/microservice/api_gateway/internal/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"InterestingChats/backend/api_gateway/internal/proxy"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -28,8 +29,8 @@ func (s *Server) RegisterRoutes() {
 	fmt.Println("Registering routes...")
 	s.rMux.HandleFunc("/registration", proxy.ProxyRequest("http://localhost:8001")).Methods("POST")
 	s.rMux.HandleFunc("/login", proxy.ProxyRequest("http://localhost:8001")).Methods("POST")
-	s.rMux.HandleFunc("/updateToken", proxy.ProxyRequest("http://localhost:8001")).Methods("POST")
-	fmt.Println("Route /registration registered for POST method")
-	fmt.Println("Route /login registered for POST method")
-	fmt.Println("Route /updateToken registered for POST method")
+	s.rMux.HandleFunc("/getTokens", proxy.ProxyRequest("http://localhost:8001")).Methods("GET")
+	log.Println("Route /registration registered for POST method")
+	log.Println("Route /login registered for POST method")
+	log.Println("Route /getTokens registered for POST method")
 }
