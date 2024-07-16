8a7014beaf8d35e1d15253e99501969c208df7e5
ssq0-0 <snikers1kitkat2@gmail.com>
2024-07-17 01:03:07 +0300

    New variable for JWT token generation, also added new error handling

--- backend/microservice/user_services/internal/utils/token.go
+++ backend/microservice/user_services/internal/utils/token.go
@@ -10,23 +10,24 @@ import (
 var jwtKey = []byte("secret")
 
 type Claims struct {
-	Username string `json:"username"`
+	Email string `json:"email"`
+
 	jwt.StandardClaims
 }
 
-func GenerateJWT(username string) (string, string, error) {
+func GenerateJWT(email string) (string, string, error) {
 	expirationAccessTime := time.Now().Add(5 * time.Minute)
 	expirationRefreshTime := time.Now().Add(1 * time.Hour)
 
 	accessClaims := &Claims{
-		Username: username,
+		Email: email,
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: expirationAccessTime.Unix(),
 		},
 	}
 
 	refreshClaims := &Claims{
-		Username: username,
+		Email: email,
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: expirationRefreshTime.Unix(),
 		},
@@ -47,17 +48,33 @@ func GenerateJWT(username string) (string, string, error) {
 	return accessTokenString, refreshTokenString, nil
 }
 
-func ValidateJWT(tokenString string) (*Claims, error) {
+func ValidateJWT(tokenString string) (string, error) {
 	claims := &Claims{}
 
 	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
 		return jwtKey, nil
 	})
-	if err != nil || !token.Valid {
-		return nil, errors.New("invalid token")
+
+	if err != nil {
+		if ve, ok := err.(*jwt.ValidationError); ok {
+			if ve.Errors&jwt.ValidationErrorMalformed != 0 {
+				return "", errors.New("token is malformed")
+			} else if ve.Errors&jwt.ValidationErrorExpired != 0 {
+				return "", errors.New("token is expired")
+			} else if ve.Errors&jwt.ValidationErrorNotValidYet != 0 {
+				return "", errors.New("token is not valid yet")
+			} else {
+				return "", errors.New("could not handle this token: " + err.Error())
+			}
+		}
+		return "", errors.New("could not handle this token: " + err.Error())
 	}
 
-	return claims, nil
+	if !token.Valid {
+		return "", errors.New("invalid token")
+	}
+
+	return claims.Email, nil
 }
 
 func RefreshToken(refreshToken string) (string, error) {
@@ -70,7 +87,7 @@ func RefreshToken(refreshToken string) (string, error) {
 		return "", errors.New("invalid token")
 	}
 
-	newAccessToken, _, err := GenerateJWT(claims.Username)
+	newAccessToken, _, err := GenerateJWT(claims.Email)
 	if err != nil {
 		return "", err
 	}
