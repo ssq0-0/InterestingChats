892fc3d6d4b9f086e98b8e2341eeea49ad3753ec
ssq0-0 <snikers1kitkat2@gmail.com>
2024-07-17 00:40:11 +0300

    Adding models for data serialisation.

--- backend/microservice/chat_service/internal/models/models.go
+++ backend/microservice/chat_service/internal/models/models.go
@@ -1,1 +1,36 @@
 package models
+
+import (
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+type User struct {
+	ID       int                  `json:"id"`
+	Username string               `json:"username"`
+	Email    string               `json:"email"`
+	Chats    map[int]UserChatInfo `json:"chats,omitempty"`
+}
+
+type UserChatInfo struct {
+	Messages []Message
+}
+
+type Message struct {
+	ID     int       `json:"id"`
+	Body   string    `json:"body"`
+	Time   time.Time `json:"time"`
+	UserID int       `json:"user_id"`
+}
+
+type Chat struct {
+	ID        int    `json:"id"`
+	ChatName  string `json:"chat_name"`
+	Members   map[int]User
+	Messages  []Message
+	Clients   map[*websocket.Conn]bool `json:"-"`
+	Broadcast chan Message             `json:"-"`
+	Mu        *sync.Mutex              `json:"-"`
+}
