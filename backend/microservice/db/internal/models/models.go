d08d6fad4271d6602ae54e8dcf6d2abb60a23f3a
ssq0-0 <snikers1kitkat2@gmail.com>
2024-07-17 00:54:41 +0300

    Added new models for processing requests coming from the 'chat_service' microservice

--- backend/microservice/db/internal/models/models.go
+++ backend/microservice/db/internal/models/models.go
@@ -1,8 +1,24 @@
 package models
 
+import "time"
+
 type User struct {
 	ID       int    `json:"id,omitempty"`
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Password string `json:"password,omitempty"`
+}
+
+type Message struct {
+	ID     int       `json:"id"`
+	Body   string    `json:"body"`
+	Time   time.Time `json:"time"`
+	UserID int       `json:"user_id"`
+}
+
+type Chat struct {
+	ID       int          `json:"id"`
+	ChatName string       `json:"chat_name"`
+	Members  map[int]User `json:"members"`
+	Messages []Message    `json:"messages"`
 }
