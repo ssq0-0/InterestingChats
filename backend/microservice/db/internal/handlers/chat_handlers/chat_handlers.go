910dfd82518e5214c763317a9053c75af6cc383a
ssq0-0 <snikers1kitkat2@gmail.com>
2024-07-24 17:00:44 +0300

    New endpoints, more optimised and logical operation of endpoints

--- backend/microservice/db/internal/handlers/chat_handlers/chat_handlers.go
+++ backend/microservice/db/internal/handlers/chat_handlers/chat_handlers.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
 )
 
 type ChatHandler struct {
@@ -15,6 +14,9 @@ type ChatHandler struct {
 }
 
 func NewChatHandler(chatService *db.ChatService) *ChatHandler {
+	if chatService == nil {
+		log.Fatal("ChatService is nil")
+	}
 	return &ChatHandler{
 		ChatService: chatService,
 	}
@@ -46,19 +48,22 @@ func (ch *ChatHandler) CreateChat(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err := ch.ChatService.CreateChat(&chat)
+	chatID, err := ch.ChatService.CreateChat(&chat)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		log.Printf("failed create chat: %v", err)
 		return
 	}
+	chat.ID = chatID
 
+	log.Printf("chat id in handler after func: %d. Func return: %d", chat.ID, chatID)
 	w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(chat); err != nil {
 		http.Error(w, "error encode json", http.StatusInternalServerError)
 		log.Println("error encoding json for response", err)
 	}
+	log.Printf("chat struct: %+v", chat)
 }
 
 func (ch *ChatHandler) DeleteChat(w http.ResponseWriter, r *http.Request) {
@@ -85,36 +90,18 @@ func (ch *ChatHandler) DeleteChat(w http.ResponseWriter, r *http.Request) {
 }
 
 func (ch *ChatHandler) DeleteMember(w http.ResponseWriter, r *http.Request) {
-	userIDsStr := r.URL.Query().Get("userID")
-	chatIDstr := r.URL.Query().Get("chatID")
-	if userIDsStr == "" || chatIDstr == "" {
-		http.Error(w, "missing user id in request", http.StatusBadRequest)
-		log.Printf("missing user id")
+	var deleteMember models.AddMemberRequest
+	if err := json.NewDecoder(r.Body).Decode(&deleteMember); err != nil {
+		log.Printf("error decoding from request body: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	chatIDint, err := strconv.Atoi(chatIDstr)
+	err := ch.ChatService.DeleteMember(deleteMember.ChatID, deleteMember.UserID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
-		log.Println("failed chatID convert to int")
+		log.Printf("failed delete from db: %v", err)
 		return
-	}
-
-	userIDs := strings.Split(userIDsStr, ",")
-	for _, userIDStr := range userIDs {
-		userIDint, err := strconv.Atoi(userIDStr)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			log.Println("failed userID convert to int")
-			return
-		}
-
-		err = ch.ChatService.DeleteMember(chatIDint, userIDint)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			log.Println("failed delete from db")
-			return
-		}
 	}
 
 	log.Println("user deleted from chat")
@@ -122,39 +109,55 @@ func (ch *ChatHandler) DeleteMember(w http.ResponseWriter, r *http.Request) {
 }
 
 func (ch *ChatHandler) AddMembers(w http.ResponseWriter, r *http.Request) {
-	userIDsStr := r.URL.Query().Get("userID")
-	chatIDstr := r.URL.Query().Get("chatID")
+	var addMember models.AddMemberRequest
+	if err := json.NewDecoder(r.Body).Decode(&addMember); err != nil {
+		log.Printf("error decode json to array models: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-	if userIDsStr == "" || chatIDstr == "" {
-		http.Error(w, "missing chat id or user id's", http.StatusBadRequest)
-		log.Printf("missing user id or chat id")
+	err := ch.ChatService.AddMember(addMember.ChatID, addMember.UserID)
+	if err != nil {
+		log.Printf("failed operation: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	log.Println("user add to chat")
+	w.WriteHeader(http.StatusAccepted)
+}
+
+func (ch *ChatHandler) GetAuthor(w http.ResponseWriter, r *http.Request) {
+	if ch.ChatService == nil {
+		log.Fatal("ChatService is nil")
+	}
+
+	userEmail := r.URL.Query().Get("email")
+	chatIDstr := r.URL.Query().Get("chatID")
+	if userEmail == "" || chatIDstr == "" {
+		http.Error(w, "missing email or chat name", http.StatusBadRequest)
+		log.Printf("missing email or chat name")
 		return
 	}
 
 	chatIDint, err := strconv.Atoi(chatIDstr)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
-		log.Println("failed chatID convert to int")
+		log.Printf("failed conver chat id to int: %v", err)
 		return
 	}
 
-	userIDArray := strings.Split(userIDsStr, ",")
-	for _, userIDstr := range userIDArray {
-		userIDint, err := strconv.Atoi(userIDstr)
-		if err != nil {
-			log.Printf("failed convert to int: %v", err)
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		err = ch.ChatService.AddMember(chatIDint, userIDint)
-		if err != nil {
-			log.Printf("failed operation: %v", err)
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
+	accept, err := ch.ChatService.CheckAuthor(userEmail, chatIDint)
+	if err != nil {
+		http.Error(w, "error checking author", http.StatusForbidden)
+		log.Printf("error checking author: %v", err)
+		return
+	}
+	if !accept {
+		http.Error(w, "user is not the author", http.StatusForbidden)
+		log.Printf("user email: %s is not the author of chat %d", userEmail, chatIDint)
+		return
 	}
 
-	log.Println("user add to chat")
-	w.WriteHeader(http.StatusAccepted)
+	w.WriteHeader(http.StatusOK)
 }
--- backend/microservice/db/internal/handlers/user/user_handlers.go
+++ backend/microservice/db/internal/handlers/user/user_handlers.go
@@ -7,21 +7,22 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
-type Handler struct {
+type UserHandler struct {
 	Db *sql.DB
 }
 
-func NewHandler(db *sql.DB) *Handler {
-	return &Handler{
+func NewHandler(db *sql.DB) *UserHandler {
+	return &UserHandler{
 		Db: db,
 	}
 }
 
-func (h *Handler) Registrations(w http.ResponseWriter, r *http.Request) {
+func (h *UserHandler) Registrations(w http.ResponseWriter, r *http.Request) {
 	var u models.User
 	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -32,7 +33,10 @@ func (h *Handler) Registrations(w http.ResponseWriter, r *http.Request) {
 	userService := db.NewUserService(h.Db)
 	err := userService.CreateNewUser(r.Context(), u)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		/*
+			Figure out how to return not only StatusConfilct, but error by situation
+		*/
+		http.Error(w, err.Error(), http.StatusConflict)
 		log.Println("Can't exec user in database: ", err)
 		return
 	}
@@ -43,10 +47,10 @@ func (h *Handler) Registrations(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
-func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
+func (h *UserHandler) Login(w http.ResponseWriter, r *http.Request) {
 	var u models.User
 	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
-		log.Println("error decode: ", err)
+		log.Println("Error decoding user login data:", err)
 		http.Error(w, "Failed to decode user login data", http.StatusBadRequest)
 		return
 	}
@@ -54,18 +58,53 @@ func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
 	userService := db.NewUserService(h.Db)
 	dbPassword, err := userService.LoginData(r.Context(), u)
 	if err != nil {
-		log.Println("Incorrect user data decode: ", err)
-		http.Error(w, "Incorrect user data:", http.StatusBadRequest)
+		log.Println("Error fetching user data from database:", err)
+		http.Error(w, "Incorrect user data", http.StatusBadRequest)
 		return
 	}
+
 	if err := bcrypt.CompareHashAndPassword([]byte(dbPassword), []byte(u.Password)); err != nil {
-		log.Println("Incoerrect email or password:", err)
+		log.Println("Incorrect email or password:", err)
 		http.Error(w, "Incorrect email or password", http.StatusBadRequest)
 		return
 	}
+
 	response := map[string]string{
 		"message": "Successful login!",
 	}
 	w.WriteHeader(http.StatusAccepted)
 	json.NewEncoder(w).Encode(response)
+}
+
+func (h *UserHandler) CheckUser(w http.ResponseWriter, r *http.Request) {
+	userIDstr := r.URL.Query().Get("userID")
+	if userIDstr == "" {
+		log.Println("missing user id")
+		http.Error(w, "missing user id n request", http.StatusBadRequest)
+		return
+	}
+
+	userIDint, err := strconv.Atoi(userIDstr)
+	if err != nil {
+		log.Printf("error in user service: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// срочно переделать на прокидывание сервиса в хендлер
+	userService := db.NewUserService(h.Db)
+	accept, err := userService.CheckUser(userIDint)
+	if err != nil {
+		log.Printf("failed request: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if !accept {
+		log.Println("user not found")
+		http.Error(w, "user not found", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
