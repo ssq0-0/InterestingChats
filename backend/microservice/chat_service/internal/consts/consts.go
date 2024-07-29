ccd94d7161a4e28f38bd2f0295cab09ae6f57840
ssq0-0 <snikers1kitkat2@gmail.com>
2024-07-30 00:50:21 +0300

    Added new response model

--- backend/microservice/chat_service/internal/consts/consts.go
+++ backend/microservice/chat_service/internal/consts/consts.go
@@ -5,13 +5,14 @@ const (
 	POST_Method   = "POST"
 	DELETE_Method = "DELETE"
 
-	DB_GetChatHistory = "http://localhost:8002/getChat?chatName=%s"
-	DB_CreateChat     = "http://localhost:8002/createChat"
-	DB_DeleteChat     = "http://localhost:8002/deleteChat?chatID=%s"
-	DB_GetAuthor      = "http://localhost:8002/getAuthor?email=%s&chatID=%d"
-	DB_AddMembers     = "http://localhost:8002/addMembers"
-	DB_CheckUser      = "http://localhost:8002/checkUser?userID=%d"
-	DB_DeleteMember   = "http://localhost:8002/deleteMember"
+	DB_GetChatHistory   = "http://localhost:8002/getChat?chatName=%s"
+	DB_CreateChat       = "http://localhost:8002/createChat"
+	DB_DeleteChat       = "http://localhost:8002/deleteChat?chatID=%d"
+	DB_GetAuthor        = "http://localhost:8002/getAuthor?userID=%d&chatID=%d"
+	DB_AddMembers       = "http://localhost:8002/addMembers"
+	DB_ChatMemberExists = "http://localhost:8002/chatMemberExists?email=%s"
+	DB_CheckUser        = "http://localhost:8002/checkUser?userID=%d"
+	DB_DeleteMember     = "http://localhost:8002/deleteMember"
 
 	US_CheckToken = "http://localhost:8001/checkToken?token=%s"
 )
