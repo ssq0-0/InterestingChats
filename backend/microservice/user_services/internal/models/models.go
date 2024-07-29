6e0884c7ee06a1f5ccf2c3e6f3f2038b4f89fd55
ssq0-0 <snikers1kitkat2@gmail.com>
2024-07-30 01:02:17 +0300

    Added new response model

--- backend/microservice/user_services/internal/models/models.go
+++ backend/microservice/user_services/internal/models/models.go
@@ -19,3 +19,8 @@ type Tokens struct {
 type UserTokens struct {
 	Tokens Tokens `json:"tokens"`
 }
+
+type Response struct {
+	Errors []string    `json:"Errors"`
+	Data   interface{} `json:"Data"`
+}
