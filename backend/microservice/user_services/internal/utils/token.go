66736ece367ad5874e9c500beafdce92a9f93445
ssq0-0 <snikers1kitkat2@gmail.com>
2024-07-30 01:05:01 +0300

    Token formation has been changed. Now instead of Email the token contains all information about the user that is currently implemented (Email, Username, ID). ID is returned for all token requests.

--- backend/microservice/user_services/internal/utils/token.go
+++ backend/microservice/user_services/internal/utils/token.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"InterestingChats/backend/user_services/internal/models"
 	"errors"
 	"time"
 
@@ -10,24 +11,29 @@ import (
 var jwtKey = []byte("secret")
 
 type Claims struct {
-	Email string `json:"email"`
-
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Username string `json:"username"`
 	jwt.StandardClaims
 }
 
-func GenerateJWT(email string) (string, string, error) {
+func GenerateJWT(user models.User) (string, string, error) {
 	expirationAccessTime := time.Now().Add(5 * time.Minute)
 	expirationRefreshTime := time.Now().Add(1 * time.Hour)
 
 	accessClaims := &Claims{
-		Email: email,
+		ID:       user.ID,
+		Email:    user.Email,
+		Username: user.Username,
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: expirationAccessTime.Unix(),
 		},
 	}
 
 	refreshClaims := &Claims{
-		Email: email,
+		ID:       user.ID,
+		Email:    user.Email,
+		Username: user.Username,
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: expirationRefreshTime.Unix(),
 		},
@@ -48,7 +54,7 @@ func GenerateJWT(email string) (string, string, error) {
 	return accessTokenString, refreshTokenString, nil
 }
 
-func ValidateJWT(tokenString string) (string, error) {
+func ValidateJWT(tokenString string) (int, error) {
 	claims := &Claims{}
 
 	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
@@ -57,39 +63,39 @@ func ValidateJWT(tokenString string) (string, error) {
 	if err != nil {
 		if ve, ok := err.(*jwt.ValidationError); ok {
 			if ve.Errors&jwt.ValidationErrorMalformed != 0 {
-				return "", errors.New("token is malformed")
+				return 0, errors.New("token is malformed")
 			} else if ve.Errors&jwt.ValidationErrorExpired != 0 {
-				return "", errors.New("token is expired")
+				return 0, errors.New("token is expired")
 			} else if ve.Errors&jwt.ValidationErrorNotValidYet != 0 {
-				return "", errors.New("token is not valid yet")
+				return 0, errors.New("token is not valid yet")
 			} else {
-				return "", errors.New("could not handle this token: " + err.Error())
+				return 0, errors.New("could not handle this token: " + err.Error())
 			}
 		}
-		return "", errors.New("could not handle this token: " + err.Error())
+		return 0, errors.New("could not handle this token: " + err.Error())
 	}
 
 	if !token.Valid {
-		return "", errors.New("invalid token")
+		return 0, errors.New("invalid token")
 	}
 
-	return claims.Email, nil
+	return claims.ID, nil
 }
 
-func RefreshToken(refreshToken string) (string, error) {
-	claims := &Claims{}
+// func RefreshToken(refreshToken string) (string, error) {
+// 	claims := &Claims{}
 
-	token, err := jwt.ParseWithClaims(refreshToken, claims, func(token *jwt.Token) (interface{}, error) {
-		return jwtKey, nil
-	})
-	if err != nil || !token.Valid {
-		return "", errors.New("invalid token")
-	}
+// 	token, err := jwt.ParseWithClaims(refreshToken, claims, func(token *jwt.Token) (interface{}, error) {
+// 		return jwtKey, nil
+// 	})
+// 	if err != nil || !token.Valid {
+// 		return "", errors.New("invalid token")
+// 	}
 
-	newAccessToken, _, err := GenerateJWT(claims.Email)
-	if err != nil {
-		return "", err
-	}
+// 	newAccessToken, _, err := GenerateJWT(claims.Email)
+// 	if err != nil {
+// 		return "", err
+// 	}
 
-	return newAccessToken, nil
-}
+// 	return newAccessToken, nil
+// }
