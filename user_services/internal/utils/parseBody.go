e554cf8d1f9690242f25b4a2d9bea94210f08d47
SSQ <110625524+ssq0-0@users.noreply.github.com>
2024-08-29 22:10:48 +0300

    Delete user_services directory
    Version control error

--- user_services/internal/utils/parseBody.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-// func ParseBody[T any](body []byte, result *T) error {
-// 	if err := json.Unmarshal(body, result); err != nil {
-// 		return fmt.Errorf(consts.InternalFailedParseBody, err)
-// 	}
-// 	return nil
-// }
